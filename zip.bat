64a9ff3cc34ad6400111545eb064a14538fad063
Blacksalsify <threatplates@gmail.com>
2016-09-02 20:10:37 +0200

    Change log info for 8.010-Beta

--- zip.bat
+++ zip.bat
@@ -1,2 +1,2 @@
 set version=%1
-"F:\Program Files\7-Zip\7z.exe" a -xr@exclude.lst ..\TidyPlates_ThreatPlates_%version%.zip ..\TidyPlates_ThreatPlates 
+"F:\Program Files\7-Zip\7z.exe" a -xr@exclude.lst "F:\Eigene Dateien\Dokumente\WoW\ThreatPlates\Releases\TidyPlates_ThreatPlates_%version%.zip" ..\TidyPlates_ThreatPlates 
