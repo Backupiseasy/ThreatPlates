43ab027827373bf65a40e58131ba29ec8ca160e5
Blacksalsify <threatplates@gmail.com>
2017-05-21 18:19:39 +0200

    Merge with New_Release, fixes because of Sourcetree bug

--- Styles/Styles.lua
+++ Styles/Styles.lua
@@ -15,16 +15,9 @@ local GetUnitVisibility = ThreatPlates.GetUnitVisibility
 ---------------------------------------------------------------------------------------------------
 -- Helper functions for styles and functions
 ---------------------------------------------------------------------------------------------------
-local function OnThreatTable(unit)
-  -- "is unit inactive" from TidyPlates - fast, but doesn't meant that player is on threat table
-  -- return  (unit.health < unit.healthmax) or (unit.isInCombat or unit.threatValue > 0) or (unit.isCasting == true) then
-
-  -- nil means player is not on unit's threat table - more acurate, but slower reaction time than the above solution
-  return UnitThreatSituation("player", unit.unitid)
-end
 
 -- Mapping necessary - to removed it, config settings must be changed/migrated
--- Visibility         Scale / Alpha               Threat System
+-- Visibility    Scale / Alpha               Threat System
 -------------------------------------------------------------------------
 -- FriendlyPlayer   = FriendlyPlayer
 -- FriendlyNPC      = FriendlyNPC
@@ -50,6 +43,14 @@ local function GetSimpleUnitType(unit)
   else
     return unit_type
   end
+end
+
+local function OnThreatTable(unit)
+  -- "is unit inactive" from TidyPlates - fast, but doesn't meant that player is on threat table
+  -- return  (unit.health < unit.healthmax) or (unit.isInCombat or unit.threatValue > 0) or (unit.isCasting == true) then
+
+  -- nil means player is not on unit's threat table - more acurate, but slower reaction time than the above solution
+  return UnitThreatSituation("player", unit.unitid)
 end
 
 local function ShowThreatFeedback(unit)
@@ -111,14 +112,15 @@ local function GetUnitType(unit)
   return faction, unit_class
 end
 
-
 local function ShowUnit(unit)
   local db = TidyPlatesThreat.db.profile
 
   -- If nameplate visibility is controlled by Wow itself (configured via CVars), this function is never used as
   -- nameplates aren't created in the first place (e.g. friendly NPCs, totems, guardians, pets, ...)
   local faction, unit_type = GetUnitType(unit)
-  local show, headline_view = GetUnitVisibility(faction, unit_type)
+  local full_unit_type = faction .. unit_type
+
+  local show, headline_view = GetUnitVisibility(full_unit_type)
 
   local db_hv = db.HeadlineView
   if not db_hv.ON then
@@ -143,16 +145,6 @@ local function ShowUnit(unit)
     show = false
   end
 
---  if b then
---    unit.TP_DetailedUnitType = "Boss"
---    show = not hide_b
---  elseif e then
---    unit.TP_DetailedUnitType = "Elite"
---    show = not hide_e
---  else
---    show = not hide_n
---  end
-
   if full_unit_type == "EnemyNPC" then
     if b then
       unit.TP_DetailedUnitType = "Boss"
@@ -171,31 +163,18 @@ end
 
 -- Returns style based on threat (currently checks for in combat, should not do hat)
 local function GetThreatStyle(unit)
-  local db = TidyPlatesThreat.db.profile
-
+  local db = TidyPlatesThreat.db.profile.threat
   local style = "normal"
-  local T = GetSimpleUnitType(unit)
-  local db_threat = db.threat
 
   -- style tank/dps only used for NPCs/non-player units
-  if (InCombatLockdown() and unit.type == "NPC" and unit.reaction ~= "FRIENDLY") and db_threat.ON then
-    --		if db.threat.toggle[T] then
-      if db_threat.nonCombat  then
-        if OnThreatTable(unit) then
-          if TidyPlatesThreat:GetSpecRole() then
-            style = "tank"
-          else
-            style = "dps"
-          end
-        end
+  if (InCombatLockdown() and unit.type == "NPC" and unit.reaction ~= "FRIENDLY") and db.ON then
+    if ShowThreatFeedback(unit) then
+      if TidyPlatesThreat:GetSpecRole()	then
+        style = "tank"
       else
-        if TidyPlatesThreat:GetSpecRole()	then
-          style = "tank"
-        else
-          style = "dps"
-        end
+        style = "dps"
       end
---		end
+		end
   end
 
   return style
@@ -221,6 +200,13 @@ local function GetUniqueNameplateSetting(unit)
 end
 
 local function SetStyle(unit)
+  if not unit.unitid then
+    -- sometimes unitid is nil, still don't know why, but it creates all kinds of LUA errors as other attributes are nil
+    -- also, e.g., unit.type, unit.name, ...
+    --ThreatPlates.DEBUG_PRINT_UNIT(unit)
+    return "empty"
+  end
+
   local db = TidyPlatesThreat.db.profile
   local style = "empty"
   local unique_setting
@@ -254,26 +240,16 @@ local function SetStyle(unit)
       elseif unit.reaction == "FRIENDLY" then
         style = "normal"
       else
+        -- could call GetThreatStyle here, but that would at a tiny overhead
         style = "normal"
-        local T = GetSimpleUnitType(unit)
         local db_threat = db.threat
         -- style tank/dps only used for hostile (enemy, neutral) NPCs
         if InCombatLockdown() and unit.type == "NPC" and db_threat.ON then -- and unit.reaction ~= "FRIENDLY" because of previous if-part
-          if db_threat.toggle[T] then
-            if db_threat.nonCombat  then
-              if OnThreatTable(unit) then
-                if TidyPlatesThreat:GetSpecRole() then
-                  style = "tank"
-                else
-                  style = "dps"
-                end
-              end
+          if ShowThreatFeedback(unit) then
+            if TidyPlatesThreat:GetSpecRole() then
+              style = "tank"
             else
-              if TidyPlatesThreat:GetSpecRole()	then
-                style = "tank"
-              else
-                style = "dps"
-              end
+              style = "dps"
             end
           end
         end
@@ -288,5 +264,4 @@ TidyPlatesThreat.OnThreatTable = OnThreatTable
 TidyPlatesThreat.ShowThreatFeedback = ShowThreatFeedback
 TidyPlatesThreat.GetThreatStyle = GetThreatStyle
 TidyPlatesThreat.GetUniqueNameplateSetting = GetUniqueNameplateSetting
-TidyPlatesThreat.GetSimpleUnitType = GetSimpleUnitType
 TidyPlatesThreat.SetStyle = SetStyle
