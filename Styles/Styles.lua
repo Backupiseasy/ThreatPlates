6b64831f87d6dd99aac93e5a240edd35dbd14066
Blacksalsify <threatplates@gmail.com>
2017-10-04 14:21:00 +0200

    Small optimizations in how the nameplate style is determined

--- Styles/Styles.lua
+++ Styles/Styles.lua
@@ -123,8 +123,6 @@ local function GetUnitType(unit)
 end
 
 local function ShowUnit(unit)
-  local db = TidyPlatesThreat.db.profile
-
   -- If nameplate visibility is controlled by Wow itself (configured via CVars), this function is never used as
   -- nameplates aren't created in the first place (e.g. friendly NPCs, totems, guardians, pets, ...)
   local faction, unit_type = GetUnitType(unit)
@@ -132,33 +130,20 @@ local function ShowUnit(unit)
 
   local show, headline_view = GetUnitVisibility(full_unit_type)
 
-  local unit_id = unit.unitid
-  local db_hv = db.HeadlineView
-  if not db_hv.ON then
-    headline_view = false
-  elseif db_hv.ForceHealthbarOnTarget and unit.isTarget then
-    headline_view = false
-  elseif db_hv.ForceOutOfCombat and not InCombatLockdown() then
-    headline_view = true
-  elseif db_hv.ForceNonAttackableUnits and unit.reaction ~= "FRIENDLY" and not UnitCanAttack("player", unit_id) then
-    headline_view = true
-  elseif db_hv.ForceFriendlyInCombat and unit.reaction == "FRIENDLY" and InCombatLockdown() then
-    headline_view = true
-  end
-
   local e, b, t = (unit.isElite or unit.isRare), unit.isBoss, unit.isTapped
-  local visibility = db.Visibility
-  local hide_n, hide_e, hide_b, hide_t = visibility.HideNormal , visibility.HideElite, visibility.HideBoss, visibility.HideTapped
+  local db_base = TidyPlatesThreat.db.profile
+  local db = db_base.Visibility
+  --local hide_n, hide_e, hide_b, hide_t = visibility.HideNormal , visibility.HideElite, visibility.HideBoss, visibility.HideTapped
 
-  if (e and hide_e) or (b and hide_b) or (t and hide_t) then
-    show = false
-  elseif hide_n and not (e or b) then
-    show = false
-  elseif UnitIsBattlePet(unit_id) then
+  if (e and db.HideElite) or (b and db.HideBoss) or (t and db.HideTapped) then
+    return false
+  elseif db.HideNormal and not (e or b) then
+    return false
+  elseif UnitIsBattlePet(unit.unitid) then
     -- TODO: add configuration option for enable/disable
-    show = false
-  elseif visibility.HideFriendlyInCombat and unit.reaction == "FRIENDLY" and InCombatLockdown() then
-    show = false
+    return false
+  elseif db.HideFriendlyInCombat and unit.reaction == "FRIENDLY" and InCombatLockdown() then
+    return false
   end
 
   if full_unit_type == "EnemyNPC" then
@@ -171,7 +156,24 @@ local function ShowUnit(unit)
 
   if t then
     unit.TP_DetailedUnitType = "Tapped"
-    show = not hide_t
+    show = not db.HideTapped
+  end
+
+  if not show then
+    return false
+  end
+
+  db = db_base.HeadlineView
+  if not db.ON then
+    headline_view = false
+  elseif db.ForceHealthbarOnTarget and unit.isTarget then
+    headline_view = false
+  elseif db.ForceOutOfCombat and not InCombatLockdown() then
+    headline_view = true
+  elseif db.ForceNonAttackableUnits and unit.reaction ~= "FRIENDLY" and not UnitCanAttack("player", unit_id) then
+    headline_view = true
+  elseif db.ForceFriendlyInCombat and unit.reaction == "FRIENDLY" and InCombatLockdown() then
+    headline_view = true
   end
 
   return show, unit_type, headline_view
@@ -219,54 +221,60 @@ local function SetStyle(unit)
   -- sometimes unitid is nil, still don't know why, but it creates all kinds of LUA errors as other attributes are nil
   -- also, e.g., unit.type, unit.name, ...
   --ThreatPlates.DEBUG_PRINT_UNIT(unit)
-  if not unit.unitid then return "empty" end
+  if not unit.unitid then
+    unit.TP_Style = "empty"
+    return "empty", nil
+  end
 
   FixUpdateUnitCondition(unit)
+  local show, unit_type, headline_view = ShowUnit(unit)
 
-  local db = TidyPlatesThreat.db.profile
+  if not show then
+    unit.TP_Style = "empty"
+    return "empty", nil
+  end
+
   local style = "empty"
   local unique_setting
 
-  local show, unit_type, headline_view = ShowUnit(unit)
+  local db = TidyPlatesThreat.db.profile
 
-  if show then
-    -- Check if custom nameplate should be used for the unit:
-    unique_setting = GetUniqueNameplateSetting(unit)
-    if unique_setting then
-      if unique_setting.showNameplate then
-        style = "unique"
-      elseif unique_setting.ShowHeadlineView then
-        style = (db.HeadlineView.ON and "NameOnly-Unique") or "unique"
-      end
-    elseif unit_type == "Totem" then
-      local unit_name = unit.name
-      local tS = db.totemSettings[TOTEMS[unit_name]]
-      if tS[1] then
-        -- show healthbar, show headline or show nothing
-        -- if db.totemSetting.ShowHeadlineView then
-        if db.totemSettings.hideHealthbar then
-          style = "etotem"
-        else
-          style = "totem"
-        end
-      end
-    else
-      if headline_view then
-        style = "NameOnly"
-      elseif unit.reaction == "FRIENDLY" then
-        style = "normal"
+  -- Check if custom nameplate should be used for the unit:
+  unique_setting = GetUniqueNameplateSetting(unit)
+  if unique_setting then
+    if unique_setting.showNameplate then
+      style = "unique"
+    elseif unique_setting.ShowHeadlineView then
+      style = (db.HeadlineView.ON and "NameOnly-Unique") or "unique"
+    end
+  elseif unit_type == "Totem" then
+    local unit_name = unit.name
+    local tS = db.totemSettings[TOTEMS[unit_name]]
+    if tS[1] then
+      -- show healthbar, show headline or show nothing
+      -- if db.totemSetting.ShowHeadlineView then
+      if db.totemSettings.hideHealthbar then
+        style = "etotem"
       else
-        -- could call GetThreatStyle here, but that would at a tiny overhead
-        style = "normal"
-        local db_threat = db.threat
-        -- style tank/dps only used for hostile (enemy, neutral) NPCs
-        if InCombatLockdown() and unit.type == "NPC" and db_threat.ON then -- and unit.reaction ~= "FRIENDLY" because of previous if-part
-          if ShowThreatFeedback(unit) then
-            if TidyPlatesThreat:GetSpecRole() then
-              style = "tank"
-            else
-              style = "dps"
-            end
+        style = "totem"
+      end
+    end
+  else
+    if headline_view then
+      style = "NameOnly"
+    elseif unit.reaction == "FRIENDLY" then
+      style = "normal"
+    else
+      -- could call GetThreatStyle here, but that would at a tiny overhead
+      style = "normal"
+      local db_threat = db.threat
+      -- style tank/dps only used for hostile (enemy, neutral) NPCs
+      if InCombatLockdown() and unit.type == "NPC" and db_threat.ON then -- and unit.reaction ~= "FRIENDLY" because of previous if-part
+        if ShowThreatFeedback(unit) then
+          if TidyPlatesThreat:GetSpecRole() then
+            style = "tank"
+          else
+            style = "dps"
           end
         end
       end
