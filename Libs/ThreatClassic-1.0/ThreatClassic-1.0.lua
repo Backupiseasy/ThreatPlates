ae19df2f68b8d345650f1e43f5acdf43b6867ca0
Blacksalsify <threatplates@gmail.com>
2019-09-02 12:29:52 +0200

    Updated lib Threat Classic to rev 4.

--- Libs/ThreatClassic-1.0/ThreatClassic-1.0.lua
+++ Libs/ThreatClassic-1.0/ThreatClassic-1.0.lua
@@ -55,7 +55,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 -------------------------------------------------------------------------------]]
 
 local MAJOR_VERSION = "ThreatClassic-1.0"
-local MINOR_VERSION = 3
+local MINOR_VERSION = 4
 
 if MINOR_VERSION > _G.ThreatLib_MINOR_VERSION then _G.ThreatLib_MINOR_VERSION = MINOR_VERSION end
 
