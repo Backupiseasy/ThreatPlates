15d1f8da6873e6b4bb2dc51ae62e1f7a862bf798
Blacksalsify <threatplates@gmail.com>
2019-10-01 18:38:04 +0200

    Fixed a bug where the player pet's nameplate was only shown when Guardian nameplates were enabled (not pet nameplates) [E-Mail].

--- Styles/Styles.lua
+++ Styles/Styles.lua
@@ -7,7 +7,7 @@ local ThreatPlates = Addon.ThreatPlates
 
 -- WoW APIs
 local InCombatLockdown = InCombatLockdown
-local UnitPlayerControlled = UnitPlayerControlled
+local UnitPlayerControlled, UnitIsUnit = UnitPlayerControlled, UnitIsUnit
 local UnitIsOtherPlayersPet = UnitIsOtherPlayersPet
 local UnitIsBattlePet = UnitIsBattlePet
 local UnitCanAttack, UnitIsTapDenied = UnitCanAttack, UnitIsTapDenied
@@ -83,7 +83,7 @@ local function GetUnitType(unit)
     unit_class = "Player"
   elseif unit.TotemSettings then
     unit_class = "Totem"
-  elseif UnitIsOtherPlayersPet(unit.unitid) then -- player pets are also considered guardians, so this check has priority
+  elseif UnitIsOtherPlayersPet(unit.unitid) or UnitIsUnit(unit.unitid, "pet") then -- player pets are also considered guardians, so this check has priority
     unit_class = "Pet"
   elseif UnitPlayerControlled(unit.unitid) then
     unit_class = "Guardian"
