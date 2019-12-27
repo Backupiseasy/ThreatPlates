a71b52c78e4f5a8966ef62ae0e9acf42d3cf0afe
Blacksalsify <threatplates@gmail.com>
2019-12-27 15:58:08 +0100

    Updated included libraries threat library to LibThreatClassic2 (v2.07) (Issue #24).

--- Init.lua
+++ Init.lua
@@ -23,7 +23,7 @@ ThreatPlates.L = LibStub("AceLocale-3.0"):GetLocale("TidyPlatesThreat")
 ThreatPlates.Media = LibStub("LibSharedMedia-3.0")
 Addon.LibCustomGlow = LibStub("LibCustomGlow-1.0")
 Addon.LibAceConfigDialog = LibStub("AceConfigDialog-3.0")
-Addon.LibThreatClassic = LibStub:GetLibrary("ThreatClassic-1.0")
+Addon.LibThreatClassic = LibStub:GetLibrary("LibThreatClassic2")
 Addon.LibClassicCasterino = LibStub("LibClassicCasterino-ThreatPlates")
 --Addon.LibClassicCasterino = LibStub("LibClassicCasterino")
 
