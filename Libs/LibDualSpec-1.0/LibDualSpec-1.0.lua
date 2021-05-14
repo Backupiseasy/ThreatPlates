ada7cbd7c3805324589a23621fdb1564bb632523
Blacksalsify <threatplates@gmail.com>
2021-05-14 14:28:08 +0200

    LibDualSpec is not used in TBC Classic, but in development environments still loaded. So, just don't load any code.

--- Libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
+++ Libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
@@ -32,7 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --]]
 
 -- just bail out on classic, there is no DualSpec there
-if WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then return end
+if WOW_PROJECT_ID == WOW_PROJECT_CLASSIC or WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then return end
 
 local MAJOR, MINOR = "LibDualSpec-1.0", 19
 assert(LibStub, MAJOR.." requires LibStub")
