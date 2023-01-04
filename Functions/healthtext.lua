d71667a33d6562a8bc3bfc65f1a9f6becf1345f4
Backup <threatplates@gmail.com>
2023-01-04 17:31:43 +0100

    * Fixed Lua error that could occur when scanning an invalid tooltip, e.g., for quest information [Issue #609].

--- Functions/healthtext.lua
+++ Functions/healthtext.lua
@@ -85,27 +85,29 @@ local function GetUnitSubtitle(unit)
 
 	if not subtitle then
 		local tooltip_data = C_TooltipInfo_GetUnit(unit.unitid)
-    TooltipSurfaceArgs(tooltip_data)
-    
-    if #tooltip_data.lines >= 2 then 
-      TooltipSurfaceArgs(tooltip_data.lines[1])
-      TooltipSurfaceArgs(tooltip_data.lines[2])
-
-      name = tooltip_data.lines[1].leftText
-
-      if name then name = gsub( gsub( (name), "|c........", "" ), "|r", "" ) else return end	-- Strip color escape sequences: "|c"
-      if name ~= UnitName(unit.unitid) then return end	-- Avoid caching information for the wrong unit
-
-      -- Tooltip Format Priority: Faction, Description, Level
-      local tooltip_subtitle = tooltip_data.lines[2].leftText or ""
-
-      if string.match(tooltip_subtitle, UNIT_LEVEL_TEMPLATE) then
-        subtitle = ""
-      else
-        subtitle = tooltip_subtitle
-      end
+    if tooltip_data then
+      TooltipSurfaceArgs(tooltip_data)
       
-      UnitSubtitles[name] = subtitle
+      if #tooltip_data.lines >= 2 then 
+        TooltipSurfaceArgs(tooltip_data.lines[1])
+        TooltipSurfaceArgs(tooltip_data.lines[2])
+
+        name = tooltip_data.lines[1].leftText
+
+        if name then name = gsub( gsub( (name), "|c........", "" ), "|r", "" ) else return end	-- Strip color escape sequences: "|c"
+        if name ~= UnitName(unit.unitid) then return end	-- Avoid caching information for the wrong unit
+
+        -- Tooltip Format Priority: Faction, Description, Level
+        local tooltip_subtitle = tooltip_data.lines[2].leftText or ""
+
+        if string.match(tooltip_subtitle, UNIT_LEVEL_TEMPLATE) then
+          subtitle = ""
+        else
+          subtitle = tooltip_subtitle
+        end
+        
+        UnitSubtitles[name] = subtitle
+      end
     end
 	end
 
