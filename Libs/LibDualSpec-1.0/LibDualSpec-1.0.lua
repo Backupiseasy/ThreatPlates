253da2db5e14f3a79ada510e4e313b0b0bf930b6
Backup <pongftw@googlemail.com>
2024-07-22 18:19:11 +0200

    * Updated integrated libraries (LibSharedMedia-3.0 v10.2.3, LibDualSpec-1.0 v1.24.0).

--- Libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
+++ Libs/LibDualSpec-1.0/LibDualSpec-1.0.lua
@@ -34,7 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 -- Only load in Classic Era on Season of Discovery realms
 if WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and C_Seasons.GetActiveSeason() ~= 2 then return end
 
-local MAJOR, MINOR = "LibDualSpec-1.0", 22
+local MAJOR, MINOR = "LibDualSpec-1.0", 24
 assert(LibStub, MAJOR.." requires LibStub")
 local lib, minor = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
@@ -347,10 +347,10 @@ for i = 1, numSpecs do
 			local highPointsSpentIndex = nil
 			for treeIndex = 1, 3 do
 				local name, pointsSpent, previewPointsSpent, _
-				if WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC then
-					_, name, _, _, pointsSpent, _, previewPointsSpent = GetTalentTabInfo(treeIndex, nil, nil, specIndex)
-				else
+				if WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC then
 					name, _, pointsSpent, _, previewPointsSpent = GetTalentTabInfo(treeIndex, nil, nil, specIndex)
+				else
+					_, name, _, _, pointsSpent, _, previewPointsSpent = GetTalentTabInfo(treeIndex, nil, nil, specIndex)
 				end
 				if name then
 					local displayPointsSpent = pointsSpent + previewPointsSpent
