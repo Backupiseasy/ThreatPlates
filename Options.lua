0ce8cd665b86bce77492d381b27a5b8d5757e899
Backup <pongftw@googlemail.com>
2025-06-15 17:44:15 +0200

    * Fixed a bug in the script editor that caused updates to script code to be applied to the wrong custom style. * Fixed a bug that prevented some nameplate UI elements from being updated when script code was updated.

--- Options.lua
+++ Options.lua
@@ -8951,8 +8951,7 @@ CreateCustomNameplateEntry = function(index)
                 custom_style.Scripts.Event = nil
               end
 
-              Addon:InitializeCustomNameplates()
-              Addon.Widgets:UpdateSettings("Script")
+              UpdateSpecial()
             end,
             get = function(info)
               return CustomPlateGetExampleForEventScript(db.uniqueSettings[index], db.uniqueSettings[index].Scripts.Function)
@@ -9028,7 +9027,7 @@ CreateCustomNameplateEntry = function(index)
                     val = nil
                   end
 
-                  local custom_style = db.uniqueSettings[index]
+                  local custom_style = db.uniqueSettings[Addon.ScriptEditor.SelectedIndex]
                   if custom_style.Scripts.Function == "WoWEvent" then
                     custom_style.Scripts.Code.Events[custom_style.Scripts.Event] = val
                   elseif custom_style.Scripts.Function == "Legacy" then
@@ -9042,9 +9041,7 @@ CreateCustomNameplateEntry = function(index)
                     custom_style.Scripts.Event = nil
                   end
 
-                  Addon:InitializeCustomNameplates()
-                  Addon.Widgets:UpdateSettings("Script")
-
+                  UpdateSpecial()
                   frame:Hide()
                   Addon.LibAceConfigDialog:Open(t.ADDON_NAME)
                 end
@@ -9057,6 +9054,7 @@ CreateCustomNameplateEntry = function(index)
                 label = "Event: " .. db.uniqueSettings[index].Scripts.Function
               end
               Addon.ScriptEditor.Editor:SetLabel(label)
+              Addon.ScriptEditor.SelectedIndex = index
 
               Addon.ScriptEditor.Editor:SetText(CustomPlateGetExampleForEventScript(db.uniqueSettings[index], db.uniqueSettings[index].Scripts.Function))
 
