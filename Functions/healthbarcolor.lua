5f5f3c56e34586d11569c7f5ee271e6752a3d4ad
PolyBadger <polybadger1337@gmail.com>
2025-08-12 13:18:54 -0600

    Allow custom border color on main & custom healthbars

--- Functions/healthbarcolor.lua
+++ Functions/healthbarcolor.lua
@@ -398,7 +398,26 @@ function Addon:SetHealthbarColor(unit)
     color_bg_r, color_bg_g, color_bg_b, bg_alpha = color.r, color.g, color.b, 1 - db_healthbar.BackgroundOpacity
   end
 
-  return color_r, color_g, color_b, nil, color_bg_r, color_bg_g, color_bg_b, bg_alpha
+  -- For simplicity, border color is uneffected by marks, threat, etc.
+  local border_r, border_g, border_b = 0, 0, 0  
+  if style == "unique" then
+    if unique_setting.useBorderColor then
+      -- 100% color values are not saved in the database
+      border_r = unique_setting.borderColor.r or 1
+      border_g = unique_setting.borderColor.g or 1
+      border_b = unique_setting.borderColor.b or 1
+    end
+  elseif db_healthbar.BorderUseForegroundColor then
+    border_r = color_r
+    border_g = color_g
+    border_b = color_b
+  else
+    border_r = db_healthbar.BorderColor.r or 1
+    border_g = db_healthbar.BorderColor.g or 1
+    border_b = db_healthbar.BorderColor.b or 1
+  end
+
+  return color_r, color_g, color_b, nil, color_bg_r, color_bg_g, color_bg_b, bg_alpha, border_r, border_g, border_b
 end
 
 ThreatPlates.GetColorByHealthDeficit = GetColorByHealthDeficit
