e1b369ef262d2dda89be564e72b0e4870ee2953b
Backupiseasy <Backupiseasy@users.noreply.github.com>
2025-08-14 17:20:25 +0200

    * Fixed a bug that prevented the castbar from hiding reliably for non-interruptible casts after a stun [GH-599].

--- TidyPlatesInternal/TidyPlatesCore.lua
+++ TidyPlatesInternal/TidyPlatesCore.lua
@@ -268,7 +268,7 @@ local UpdateIndicator_CustomScale, UpdateIndicator_CustomScaleText, UpdateIndica
 local UpdateIndicator_Level, UpdateIndicator_RaidIcon
 local UpdateIndicator_EliteIcon, UpdateIndicator_Name
 local UpdateIndicator_HealthBar
-local OnStartCasting, OnStopCasting, OnUpdateCastMidway
+local OnStartCasting, OnStopCasting, UpdateCastbar
 
 -- Event Functions
 local OnNewNameplate, OnShowNameplate, OnUpdateNameplate, OnResetNameplate
@@ -661,7 +661,7 @@ do
     UNIT_TARGET("UNIT_TARGET", unitid) -- requires tp_frame.Active, which is set in SetNameplateVisibility
     -- Call this after the plate is shown as OnStartCasting checks if the plate is shown; if not, the castbar is hidden and
     -- nothing is updated
-    OnUpdateCastMidway(plate, unitid)
+    UpdateCastbar(plate, unitid)
  end
 
 	-- OnUpdateNameplate
@@ -673,7 +673,7 @@ do
 		--Addon:UpdateUnitIdentity(plate.TPFrame, unitid)
     Addon:UpdateUnitContext(unit, unitid)
     ProcessUnitChanges()
-    OnUpdateCastMidway(plate, unitid)
+    UpdateCastbar(plate, unitid)
 	end
 
 	-- OnHealthUpdate
@@ -686,7 +686,7 @@ do
 
       Addon:UpdateUnitCondition(unit, unitid)
       ProcessUnitChanges()
-      OnUpdateCastMidway(nameplate, unit.unitid)
+      UpdateCastbar(nameplate, unit.unitid)
 
       -- Fix a bug where the overlay for non-interruptible casts was shown even for interruptible casts when entering combat while the unit was already casting
       --    if unit.isCasting and visual.castbar:IsShown()then
@@ -1136,7 +1136,7 @@ do
     castbar.IsCasting = false
     castbar.IsChanneling = false
     unit.isCasting = false
-
+    
     if Addon.UnitStyle_CastTrigger_Reset(unit) then
       ProcessUnitChanges()
     else
@@ -1145,18 +1145,17 @@ do
     end
   end    
   
-  function OnUpdateCastMidway(plate, unitid)
+  function UpdateCastbar(plate, unitid)
     if not ShowCastBars then return end
-
-    -- Check to see if there's a spell being cast
-    if UnitCastingInfo(unitid) then
-      OnStartCasting(plate, unitid, false)
-    elseif UnitChannelInfo(unitid) then
-      OnStartCasting(plate, unitid, true)
+    
+    local tp_frame = plate.TPFrame
+    if tp_frame.unit.isCasting then 
+      -- Check to see if there's a spell being cast
+      OnStartCasting(plate, unitid, tp_frame.visual.castbar.IsChanneling)
     else
       -- It would be better to check for IsInterrupted here and not hide it if that is true
       -- Not currently sure though, if that might work with the Hide() calls in OnStartCasting
-      visual.castbar:Hide()
+      tp_frame.visual.castbar:Hide()
     end
   end
 end -- End Indicator section
@@ -1590,7 +1589,7 @@ function CoreEvents:UNIT_THREAT_LIST_UPDATE(unitid)
       --        unit.threatSituation = ThreatReference[unit.threatValue]
       --        unit.isInCombat = _G.UnitAffectingCombat(unitid)
       --ProcessUnitChanges()
-      --OnUpdateCastMidway(nameplate, unit.unitid)
+      --UpdateCastbar(nameplate, unit.unitid)
     end
 
     -- UNIT_TARGET does not update correctly, so use this in in-combat situations as a work-around
@@ -1623,8 +1622,7 @@ local function UnitSpellcastMidway(event, unitid, ...)
 
   local plate = GetNamePlateForUnit(unitid)
   if plate then
-    UpdateReferences(plate)
-    OnUpdateCastMidway(plate, unitid)
+    OnStartCasting(plate, unitid, plate.TPFrame.visual.castbar.IsChanneling)
   end
 end
 
